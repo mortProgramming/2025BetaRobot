63bc9823b72efb1f571b5106117fd9edde359732
RandomDude2025 <adam.soliman@motsd.org>
2025-03-01 09:34:20 -0500

    123

--- src/main/java/frc/robot/config/constants/PhysicalConstants.java
+++ src/main/java/frc/robot/config/constants/PhysicalConstants.java
@@ -8,7 +8,7 @@ public final class PhysicalConstants{
 		public static final double L1=0;
 		public static final double L2=0;
 		public static final double L3=0;
-		public static final double L4=30;
+		public static final double L4=10;
 		public static final double Coral=0;
 		public static final double motorVoltage=12.0;
 		public static final double speedFactor=(0.5);
@@ -21,7 +21,7 @@ public final class PhysicalConstants{
 		public static final double L1=0.1;
 		public static final double L2=0.1;
 		public static final double L3=0.1;
-		public static final double L4=30;
+		public static final double L4=10;
 		public static final double coral=0.1;
 		public static final double speedFactor=(0.45);
 	}
