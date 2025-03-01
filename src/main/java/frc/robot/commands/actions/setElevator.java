aab05dd0f237e27237f1082770e061e65794640d
RandomDude2025 <adam.soliman@motsd.org>
2025-02-28 20:15:25 -0500

    123

--- src/main/java/frc/robot/commands/actions/setElevator.java
+++ src/main/java/frc/robot/commands/actions/setElevator.java
@@ -25,10 +25,11 @@ public class setElevator extends Command{
     }
     
     public void execute(){
-        elevator.setPosition(-elevator.getPIDController().calculate(elevator.getPosition(), targetPosition));
-        // positioncontroller.setSetpoint(targetPosition);
-        // double speed=positioncontroller.calculate(elevator.getPosition());
-        // elevator.setSpeed(speed);
+       // elevator.setPosition(-elevator.getPIDController().calculate(elevator.getPosition(), targetPosition));
+        positioncontroller.setSetpoint(targetPosition);
+        double speed=positioncontroller.calculate(elevator.getPosition());
+        elevator.setSpeed(speed);
+        System.out.println("Speed: " + speed+" Target Position: "+targetPosition+" Encoder Position: "+elevator.getPosition());
     }
     public boolean isFinished(){
         return false;
