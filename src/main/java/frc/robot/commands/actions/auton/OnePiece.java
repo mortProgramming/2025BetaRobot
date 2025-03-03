744a8fe7abea9333e8df036ddeb906d967816197
RandomDude2025 <adam.soliman@motsd.org>
2025-03-03 15:38:39 -0500

    Testing

--- src/main/java/frc/robot/commands/actions/auton/OnePiece.java
+++ src/main/java/frc/robot/commands/actions/auton/OnePiece.java
@@ -14,7 +14,7 @@ public class OnePiece extends SequentialCommandGroup{
                 new TimedDrive(4,0.6,0,0),
                 // new TimedDrive(2,-0.25,0,0),
                 // new TimedDrive(0,0,0,0),
-                new setElevator(30),
+                new setElevator(20),
                 new openCoralGate(),
                 // new TimedDrive(3, 0, -0.5, 0).withTimeout(3),
                 // new TimedDrive(0,0,0,0),
