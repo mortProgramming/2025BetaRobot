5c31a58cfbbe745c43b58492f8f88c7fa0cfe43c
RandomDude2025 <adam.soliman@motsd.org>
2025-03-03 14:44:22 -0500

    123

--- src/main/java/frc/robot/commands/actions/auton/Taxi.java
+++ src/main/java/frc/robot/commands/actions/auton/Taxi.java
@@ -2,42 +2,25 @@ package frc.robot.commands.actions.auton;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.actions.TimedDrive;
 import frc.robot.commands.actions.setCoralCorral;
+import frc.robot.commands.actions.setElevator;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.actions.openCoralGate;
 public class Taxi extends SequentialCommandGroup{
-    // private double time;
-    // private double x;
-    // private double y;
-    // private double omega;
     
-    // public Taxi(double time, double x, double y, double omega){
-    //     this.time = time;
-    //     this.x = x;
-    //     this.y = y;
-    //     this.omega = omega;
-    // }
     public Taxi(){
         // addCommands(new SequentialCommandGroup(new TimedDrive(time,x,y,omega)));
         addCommands(
             new SequentialCommandGroup(
-                new setCoralCorral(45).withTimeout(1),
-
+                new setCoralCorral(25).withTimeout(1),
                 new TimedDrive(3,0.4,0,0),
-
                 new TimedDrive(3,0,0,-0.5),
-
                 new TimedDrive(4,0.6,0,0),
-
                 // new TimedDrive(2,-0.25,0,0),
-
                 // new TimedDrive(0,0,0,0),
-
+                new setElevator(30),
                 new openCoralGate(),
-
-                new TimedDrive(3, 0, -0.50, 0).withTimeout(3),
-
+                new TimedDrive(3, 0, -0.5, 0).withTimeout(3),
                 new TimedDrive(0,0,0,0),
-
                 new setCoralCorral(0)
             ) 
         );
