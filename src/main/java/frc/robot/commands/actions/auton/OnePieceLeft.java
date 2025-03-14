b881fd1b031eb7fb2667d4d1684a7630fc21b72d
RandomDude2025 <adam.soliman@motsd.org>
2025-03-14 16:04:10 -0400

    123

--- src/main/java/frc/robot/commands/actions/auton/OnePieceLeft.java
+++ src/main/java/frc/robot/commands/actions/auton/OnePieceLeft.java
@@ -1,7 +1,10 @@
 package frc.robot.commands.actions.auton;
 
 import static frc.robot.config.constants.PhysicalConstants.ElevatorConstants.L1;
+import static frc.robot.config.constants.PhysicalConstants.ElevatorConstants.L4;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.actions.TimedDrive;
 import frc.robot.commands.actions.openCoralGate;
 import frc.robot.commands.actions.setCoralCorral;
@@ -10,7 +13,15 @@ public class OnePieceLeft extends SequentialCommandGroup{
     public OnePieceLeft(){
         addCommands(
             new SequentialCommandGroup(
-                //This is meant to be on the relative left side of the barge, with the bottom left side of the robot's bumper touching the bottom right side of the middle pole of the barge
+                //For setCoralCorral, 1 target position equals 0.037 on the absolute encoder
+                new TimedDrive(5,0.7,0,0).withTimeout(4),
+                new TimedDrive(0,0,0,0).withTimeout(1),
+
+                new ParallelCommandGroup(
+                    new setElevator(L4).withTimeout(4),
+                    new setCoralCorral(21).withTimeout(1)
+                ),
+                new openCoralGate().withTimeout(3)
             ) 
         );
     }
